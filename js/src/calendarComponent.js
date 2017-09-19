75a653d4466cc0a83b5e3cf9e21e1ab1ca151561
Breadood <htycm1@gmail.com>
2017-09-18 23:27:05 -0500

    refactor calendar html structure

--- js/src/calendarComponent.js
+++ js/src/calendarComponent.js
@@ -11,19 +11,19 @@ class CalendarComponent {
     }
     
     initialize() {
+        this.header = this.$el.find('div.calendar-header');
+        this.container = this.$el.find('div.calendar-container');
+        this.overlay = this.$el.find('div.calendar-overlay')
         this.initializeHeader();
         this.initializeDayBlocks();
     }
     
     initializeHeader() {
-        var header = '<ul class="box calendar-header"></ul>';
-        var container = '<ul class="calendar-container"></ul>';
-        
-        this.$el.append(header);
-        this.$el.append(container);
-        
-        this.header = this.$el.find('.calendar-header');
-        this.container = this.$el.find('.calendar-container');
+//        var header = '<ul class="box calendar-header"></ul>';
+//        var boxes = '<ul class="calendar-boxes"></ul>';
+//        
+//        this.$el.append(header);
+//        this.$el.append(boxes);
     }
     
     initializeDayBlocks() {
@@ -31,17 +31,21 @@ class CalendarComponent {
     }
     
     drawCalendar() {
-        this.header.empty();
-        this.container.empty();
-        this.header.append('<li class="member-title">Members</li>');
-        this.container.append('<li class="box member-list"></li>');
+        var header = this.header.find('ul'),
+            boxes = this.container.find('ul');
+        
+        header.empty();
+        boxes.empty();
+        
+        header.append('<li class="member-title">Members</li>');
+        boxes.append('<li class="box member-list"></li>');
         
         var today = new Date(this.startDate),
             number = 1;
         while (today < this.endDate) {
             if (today.getUTCDay() !== 0 && today.getUTCDay() !== 6) {
-                this.header.append('<li class="day-title">' + today.getUTCDayString() + '</li>');
-                this.container.append('<li class="box day"></div>');
+                header.append('<li class="day-title">' + today.getUTCDayString() + '</li>');
+                boxes.append('<li class="box day"></div>');
                 number++;
             }
             today.addDays(1);   
@@ -50,7 +54,10 @@ class CalendarComponent {
     
     addMember(member) {
         console.log(member);
-        this.container.find('.member-list').append('<div class="member">' + member + '</div>');
+        // create a new member
+        // append new member
+        this.overlay.append('<div>overlay for ' + member + '</div>')
+//        this.container.find('.member-list').append('<div class="member">' + member + '</div>');
     }
     
     changeStartDate(startDate) {
